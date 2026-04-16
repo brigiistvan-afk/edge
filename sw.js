fa6b891517ec2453a7fd2c2968f434ce859d6f40
IBAR <brigi.istvan@gmail.com>
2026-04-16 13:04:19 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,11 @@
-// Service Worker - mindig friss verzió
-const CACHE = 'planner-v14';
+// Service Worker - NO cache, mindig hálózatról
+const VERSION = 'v15';
 
-self.addEventListener('install', e => {
-  // Azonnal átveszi, nem vár a régi leállására
+self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  // Töröl MINDEN régi cache-t
   e.waitUntil(
     caches.keys()
       .then(keys => Promise.all(keys.map(k => caches.delete(k))))
@@ -15,19 +13,7 @@ self.addEventListener('activate', e => {
   );
 });
 
+// Minden kérést hálózatról tölt, nincs cache
 self.addEventListener('fetch', e => {
-  // MINDIG hálózatról tölt, cache csak ha offline
-  e.respondWith(
-    fetch(e.request, { cache: 'no-store' })
-      .then(res => {
-        // Csak ikonokat és manifest-et cache-eljük
-        const url = e.request.url;
-        if (url.includes('icon-') || url.includes('manifest')) {
-          const copy = res.clone();
-          caches.open(CACHE).then(c => c.put(e.request, copy));
-        }
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
